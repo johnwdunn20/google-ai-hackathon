dc49674a78d2de92ef90db2c647ec18f2b97ba29
John Dunn <johnwdunn20@gmail.com>
2024-04-03 16:56:55 -0700

    basic setup

---
+++ setup.py
@@ -0,0 +1,2 @@
+# Used to define versioning, package name, and other details for package distribution on PyPi
+# This file is used to build the package and upload it to PyPi so that it can be installed using pip
