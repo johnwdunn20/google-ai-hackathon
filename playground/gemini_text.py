8e8c6e2b788b96c7bb38506c103cc3c107bdab9a
John Dunn <johnwdunn20@gmail.com>
2024-04-03 17:23:55 -0700

    got access to models

--- playground/gemini_text.py
+++ playground/gemini_text.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 import os
+import pathlib
+import textwrap
+import google.generativeai as genai
 
 load_dotenv()
 
@@ -7,6 +10,13 @@ def get_res():
     API_KEY = os.getenv("GOOGLE_AI_STUDIO_API_KEY")
     if not API_KEY:
         raise ValueError("No API key found")
+    # configure gemini to use my api key
+    genai.configure(api_key=API_KEY)
+    
+    # list out the available models:
+    for model in genai.list_models():
+        if 'generateContent' in model.supported_generation_methods:
+            print(f'{model.name}: {model.description}')
     
 
 get_res()
