fae9b12dc48fc88e015b9dd0a4fa6b50ba768877
John Dunn <johnwdunn20@gmail.com>
2024-04-08 17:49:00 -0700

    added type

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ async def root():
     return {"message": "Hello World!"}
 
 @app.get("/items/{item_id}")
-async def get_items(item_id):
+async def get_items(item_id: int):
     return {'item_id: ': item_id}
