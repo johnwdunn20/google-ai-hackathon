3aa25456787491c063c6071740a6bbe88a7fbbe9
John Dunn <johnwdunn20@gmail.com>
2024-04-12 14:00:33 -0700

    pool connection

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,14 @@
 # Use SQLAlchemy to create a connection pool to the database
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import os
 
+def connect_to_db():
+    try:
+        # Create a connection pool to the database
+        POSTGRES_URL = os.getenv("POSTGRES_URL")
+        engine = create_engine(POSTGRES_URL, echo=True)
+        return engine
+    except Exception as e:
+        print('Error connecting to database: ', e)
+        return None
