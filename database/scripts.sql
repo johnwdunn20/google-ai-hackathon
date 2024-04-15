c6c09406c3ed59c2e6b9e217a0ae1a48295a696e
John Dunn <johnwdunn20@gmail.com>
2024-04-15 15:49:51 -0700

    added dummy data

--- database/scripts.sql
+++ database/scripts.sql
@@ -1,21 +1,28 @@
 -- Create a new schema called 'data'
 CREATE SCHEMA healthcare_data;
 
--- Create table 'schema_details' within the 'data' schema
-CREATE TABLE healthcare_data.schema_details (
-    id SERIAL PRIMARY KEY,
-    data_schema JSON NOT NULL,
-    use_case_id INTEGER
-);
-
 -- Create table 'use_case' within the 'data' schema
 CREATE TABLE healthcare_data.use_case (
     id SERIAL PRIMARY KEY,
-    master_schema JSON NOT NULL
+    master_schema JSONB NOT NULL
 );
 
--- Add a foreign key constraint to the 'schema_details' table
-ALTER TABLE healthcare_data.schema_details
-ADD CONSTRAINT fk_use_case
-FOREIGN KEY (use_case_id)
-REFERENCES healthcare_data.use_case (id);
+-- Create table 'schema_details' within the 'data' schema
+CREATE TABLE healthcare_data.schema_details (
+    id SERIAL PRIMARY KEY,
+    data_schema JSONB NOT NULL,
+    use_case_id INTEGER,
+    CONSTRAINT fk_use_case FOREIGN KEY (use_case_id)
+        REFERENCES healthcare_data.use_case (id)
+);
+
+-- Insert data into the 'use_case' table
+INSERT INTO healthcare_data.use_case (master_schema)
+VALUES
+('{ "patient": { "id": "int", "name": "text", "age": "int" }, "visit": { "id": "int", "date": "date" } }'::jsonb);
+
+-- Insert data into the 'schema_details' table
+INSERT INTO healthcare_data.schema_details (data_schema, use_case_id)
+VALUES
+('{ "patient": { "id": 101, "name": "John Doe", "age": 30 }, "visit": { "id": 501, "date": "2023-10-01" } }'::jsonb, 1),
+('{ "patient": { "id": 102, "name": "Jane Smith", "age": 25 }, "visit": { "id": 502, "date": "2023-10-02" } }'::jsonb, 1);
